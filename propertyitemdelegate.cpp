516001f125123a0410909a0637a8cdfa1200da2c
veni.georgiev <veni.georgiev@gmail.com>
2012-06-01 13:11:11 +0000

    Bug fix

--- propertyitemdelegate.cpp
+++ propertyitemdelegate.cpp
@@ -1,20 +1,54 @@
 #include "propertyitemdelegate.h"
 
-#include <QLineEdit>
+#include <QComboBox>
 #include <QCompleter>
+#include <QRect>
+#include <QtDebug>
 
 PropertyItemDelegate::PropertyItemDelegate(QObject *parent) :
     QItemDelegate(parent)
 {
 }
 
+QWidget* PropertyItemDelegate::createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const
+{
+    QComboBox* editor = new QComboBox(parent);
+    editor->setEditable(true);
+    editor->setInsertPolicy(QComboBox::NoInsert);
+    QStringList list = index.model()->data(index, Qt::UserRole).toStringList();
+    editor->addItems(list);
+    QCompleter* completer = new QCompleter(list, editor);
+    editor->setCompleter(completer);
+    return editor;
+}
+
 void PropertyItemDelegate::setEditorData(QWidget *editor, const QModelIndex &index) const
 {
-    QLineEdit* lineedit = static_cast<QLineEdit*>(editor);
-    if(lineedit){
-        QStringList list = index.model()->data(index, Qt::UserRole).toStringList();
-        QCompleter* completer = new QCompleter(list, lineedit);
-        lineedit->setCompleter(completer);
+    QString value = index.model()->data(index, Qt::EditRole).toString();
+
+    QComboBox *comboBox = static_cast<QComboBox*>(editor);
+    if(comboBox){
+        comboBox->setEditText(value);
     }
-    QItemDelegate::setEditorData(lineedit, index);
+    else QItemDelegate::setEditorData(editor, index);
 }
+
+void PropertyItemDelegate::setModelData(QWidget *editor, QAbstractItemModel *model, const QModelIndex &index) const
+{
+    QComboBox *comboBox = static_cast<QComboBox*>(editor);
+    if(comboBox){
+        QString value = comboBox->currentText();
+        model->setData(index, value, Qt::EditRole);
+    }
+    else QItemDelegate::setModelData(editor, model, index);
+}
+
+void PropertyItemDelegate::updateEditorGeometry(QWidget *editor, const QStyleOptionViewItem &option, const QModelIndex &index) const
+{
+    QRect rect;
+    rect.setTopLeft(option.rect.topLeft());
+    rect.setBottomRight(option.rect.bottomRight());
+    rect.setHeight(option.fontMetrics.height()+4);
+    editor->setGeometry(rect);
+}
+
--- propertyitemdelegate.h
+++ propertyitemdelegate.h
@@ -9,10 +9,15 @@ class PropertyItemDelegate : public QItemDelegate
 public:
     explicit PropertyItemDelegate(QObject *parent = 0);
 
-    //QWidget *createEditor(QWidget *parent, const QStyleOptionViewItem &option,
-    //                      const QModelIndex &index) const;
+    QWidget *createEditor(QWidget *parent, const QStyleOptionViewItem &option,
+                          const QModelIndex &index) const;
 
-    void setEditorData ( QWidget* editor, const QModelIndex& index ) const;
+    void setEditorData(QWidget *editor, const QModelIndex &index) const;
+    void setModelData(QWidget *editor, QAbstractItemModel *model,
+                           const QModelIndex &index) const;
+
+    void updateEditorGeometry(QWidget *editor,
+                           const QStyleOptionViewItem &option, const QModelIndex &index) const;
     
 };
 
